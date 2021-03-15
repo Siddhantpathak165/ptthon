823d54eef8d51c7e9d47645753ad1c7aa4009e73
Siddhantpathak165 <80679282+Siddhantpathak165@users.noreply.github.com>
2021-03-15 15:25:13 +0530

    Add files via upload

---
+++ special.py
@@ -0,0 +1,13 @@
+n=input("enter the data=")
+
+if n.isalpha() :
+        print("it is alphabet")
+    
+if n.islower() :
+            print("it is in lower case")
+if n.isupper () :
+           print("it is n upper case")
+elif n.isdigit() :
+        print("it is digit")
+else :
+        print("special symbol")
